cc81320aa066905288431e51b0c687184d5c8336
Luke Strickland <luke@axxim.net>
2012-02-16 16:24:42 -0600

    Minotar is now open source

---
+++ Minotar.php
@@ -0,0 +1,76 @@
+<?php
+
+$config['caching'] = true;
+
+class Minotar {
+	
+	public function get($username) {
+		if(!file_exists('./minecraft/skins/'.$username.'.png')) {
+			if(!Minotar::found("http://s3.amazonaws.com/MinecraftSkins/$username.png")) {
+				$img = WideImage::load("http://s3.amazonaws.com/MinecraftSkins/char.png");
+	    		$img->saveToFile("./minecraft/skins/char.png");
+	    		header("Status: 404 Not Found");
+				return 'char';
+			} else {
+				$img = WideImage::load("http://s3.amazonaws.com/MinecraftSkins/$username.png");
+	    		$img->saveToFile("./minecraft/skins/$username.png");
+	    		$helm = WideImage::load("./minecraft/skins/$username.png")->crop(40,8,8,8);
+	    		$helm->saveToFile("./minecraft/helm/$username.png");
+				$head = WideImage::load("./minecraft/skins/$username.png")->crop(8,8,8,8);
+				$head->saveToFile("./minecraft/heads/$username.png");
+	    		return $username;
+			}
+		} else {
+			return $username;
+		}
+	}
+
+	public function getFilesFromDir($dir) { 
+		$files = array(); 
+		if ($handle = opendir($dir)) { 
+			while (false !== ($file = readdir($handle))) { 
+				if ($file != "." && $file != ".." && $file != "Thumbs.db") { 
+					if(is_dir($dir.'/'.$file)) { 
+						$dir2 = $dir.'/'.$file; 
+						$files[] = getFilesFromDir($dir2); 
+					} 
+					else { 
+						$files[] = $dir.'/'.$file; 
+					} 
+				} 
+			} 
+			closedir($handle); 
+		} 
+		return Minotar::array_flat($files); 
+	} 
+
+	private function array_flat($array) { 
+		foreach($array as $a) { 
+			if(is_array($a)) { 
+				$tmp = array_merge($tmp, array_flat($a)); 
+			} else { 
+				$tmp[] = $a; 
+			} 
+		} 
+
+		return $tmp; 
+	} 
+
+
+
+	private function found($url) {
+		$handle = curl_init($url);
+        if (false === $handle) {
+            return false;
+        }
+        curl_setopt($handle, CURLOPT_HEADER, false);
+        curl_setopt($handle, CURLOPT_FAILONERROR, true);  // this works
+        curl_setopt($handle, CURLOPT_HTTPHEADER, Array("User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.15) Gecko/20080623 Firefox/2.0.0.15")); // request as if Firefox    
+        curl_setopt($handle, CURLOPT_NOBODY, true);
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, false);
+        $connectable = curl_exec($handle);
+        curl_close($handle);
+        return $connectable;
+	}
+
+}
---
+++ index.php
@@ -0,0 +1,109 @@
+<?php require 'klein.php';
+include 'wide/WideImage.php';
+include 'Minotar.php';
+//error_reporting(0);
+
+respond('/', function ($request, $response) {
+    $response->render('html/home.phtml');
+});
+
+respond('/avatar/[:username].[:format]?/[:size]?.[:formate]?', function ($request, $response) {
+    $name = $request->param('username', 'char');
+    $size = $request->param('size', 180);
+    $ext  = $request->param('format', '.png');
+    $ext  = $request->param('formate', '.png');
+    $name = explode('.', $name); $name = $name[0];
+    $size = explode('.', $size); $size = $size[0];
+    if($size >= 1000) $size = 1000;
+
+    $name = Minotar::get($name);
+
+    $img = WideImage::load("./minecraft/skins/$name.png")->crop(8,8,8,8)->resize($size);
+    $img->output($ext);
+});
+
+respond('/helm/[:username].[:format]?/[:size]?.[:formate]?', function ($request, $response) {
+    $name = $request->param('username', 'char');
+    $size = $request->param('size', 180);
+    $ext  = $request->param('format', '.png');
+    $ext  = $request->param('formate', '.png');
+    $name = explode('.', $name); $name = $name[0];
+    $size = explode('.', $size); $size = $size[0];
+    if($size >= 1000) $size = 1000;
+
+    $name = Minotar::get($name);
+	$black = 'iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAYAAADED76LAAAAFklEQVQY02NkYGD4z4AHMDEQAMNDAQAMUwEPqfUHaQAAAABJRU5ErkJggg=='; // Black 16x16 for comparison
+    $watermark = WideImage::load("./minecraft/skins/$name.png")->crop(40,8,8,8);
+	$base = WideImage::load("./minecraft/skins/$name.png")->crop(8,8,8,8);
+	if (base64_encode($watermark) != $black) {
+		$result = $base->merge($watermark);
+	} else {
+		$result = $base;
+	}
+
+    $result->resize($size)->output($ext);
+});
+
+respond('/random/[:size]?.[:format]?', function ($request, $response) {
+	$size = $request->param('size', 180);
+	$ext  = $request->param('format', '.png');
+	$size = explode('.', $size); $size = $size[0];
+	if($size >= 1000) $size = 1000;
+
+	$avatars = scandir('./minecraft/heads/');
+	$rand = array_rand($avatars);
+	$avatar = $avatars[$rand];
+
+	$name = str_replace('.png', '', $avatar);
+	header("Cache-Control: no-cache, must-revalidate");
+	header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); 
+	WideImage::load("./minecraft/heads/$name.png")->resize($size)->output('png');
+});
+
+respond('/download/[:username]', function ($request, $response) {
+	$name = $request->param('username', 'char');
+
+	$name = Minotar::get($name);
+
+	$img = WideImage::load("./minecraft/skins/$name.png");
+	header('Content-Disposition: attachment; filename="'.$name.'.png"');
+	$img->output('.png');
+
+});
+
+respond('/skin/[:username]', function ($request, $response) {
+	$name = $request->param('username', 'char');
+
+	$name = Minotar::get($name);
+
+	$img = WideImage::load("./minecraft/skins/$name.png");
+	$img->output('.png');
+});
+
+respond('/all/[:type]?', function ($request, $response) {
+	$type = $request->param('type', 'heads');
+	$files = Minotar::getFilesFromDir("./minecraft/$type");
+	$response->render('html/all.phtml', array('files' => $files));
+});
+
+respond('/wallpaper/[:width]/[:height]?', function ($request, $response) {
+	$width = $request->param('width', 1024);
+	$height = $request->param('height', 768);
+	$files = Minotar::getFilesFromDir("./minecraft/heads");
+	if($width >= 1920) $width = 1920;
+	if($height >= 1080) $height = 1080;
+	$files = array_slice($files, 500);
+
+	//list($width, $height) = getimagesize($_GET['image']);
+    $image_p = imagecreatetruecolor($width, $height);
+    $count = 1;
+    foreach($files as $avatar) {
+    	$image = imagecreatefrompng($avatar);
+    	imagecopyresampled($image_p, $image, $width * $count, $height * $count, 0, 0, $width, $height, 42, 42);
+    	$count++;
+    }
+    header('Content-type: image/png');
+    imagejpeg($image_p, null, 100);
+});
+
+dispatch(); 
