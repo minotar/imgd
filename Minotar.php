2a2a999f86a7850d7a44b731d421f159713ef726
Luke Strickland <luke@axxim.net>
2012-03-21 19:28:13 -0500

    fixed error

--- Minotar.php
+++ Minotar.php
@@ -8,17 +8,17 @@ class Minotar {
 		if(!file_exists('./minecraft/skins/'.$username.'.png')) {
 			if(!Minotar::found("http://s3.amazonaws.com/MinecraftSkins/$username.png")) {
 				$img = WideImage::load("http://s3.amazonaws.com/MinecraftSkins/char.png");
-	    		$img->saveToFile("./minecraft/skins/char.png");
-	    		header("Status: 404 Not Found");
+				$img->saveToFile("./minecraft/skins/char.png");
+				header("Status: 404 Not Found");
 				return 'char';
 			} else {
 				$img = WideImage::load("http://s3.amazonaws.com/MinecraftSkins/$username.png");
-	    		$img->saveToFile("./minecraft/skins/$username.png");
-	    		$helm = WideImage::load("./minecraft/skins/$username.png")->crop(40,8,8,8);
-	    		$helm->saveToFile("./minecraft/helm/$username.png");
+				$img->saveToFile("./minecraft/skins/$username.png");
+				$helm = WideImage::load("./minecraft/skins/$username.png")->crop(40,8,8,8);
+				$helm->saveToFile("./minecraft/helms/$username.png");
 				$head = WideImage::load("./minecraft/skins/$username.png")->crop(8,8,8,8);
 				$head->saveToFile("./minecraft/heads/$username.png");
-	    		return $username;
+				return $username;
 			}
 		} else {
 			return $username;
@@ -60,17 +60,17 @@ class Minotar {
 
 	private function found($url) {
 		$handle = curl_init($url);
-        if (false === $handle) {
-            return false;
-        }
-        curl_setopt($handle, CURLOPT_HEADER, false);
-        curl_setopt($handle, CURLOPT_FAILONERROR, true);  // this works
-        curl_setopt($handle, CURLOPT_HTTPHEADER, Array("User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.15) Gecko/20080623 Firefox/2.0.0.15")); // request as if Firefox    
-        curl_setopt($handle, CURLOPT_NOBODY, true);
-        curl_setopt($handle, CURLOPT_RETURNTRANSFER, false);
-        $connectable = curl_exec($handle);
-        curl_close($handle);
-        return $connectable;
+		if (false === $handle) {
+			return false;
+		}
+		curl_setopt($handle, CURLOPT_HEADER, false);
+		curl_setopt($handle, CURLOPT_FAILONERROR, true);  // this works
+		curl_setopt($handle, CURLOPT_HTTPHEADER, Array("User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.15) Gecko/20080623 Firefox/2.0.0.15")); // request as if Firefox    
+		curl_setopt($handle, CURLOPT_NOBODY, true);
+		curl_setopt($handle, CURLOPT_RETURNTRANSFER, false);
+		$connectable = curl_exec($handle);
+		curl_close($handle);
+		return $connectable;
 	}
 
 }
