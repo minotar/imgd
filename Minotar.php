d2b2e0923d283aba3ec4834a01e3c3aceed3d729
Kevin Ingersoll <kingersoll@gmail.com>
2012-06-02 05:49:11 -0700

    Convert to ImageMagick and clean up logic

--- Minotar.php
+++ Minotar.php
@@ -3,30 +3,40 @@
 $config['caching'] = true;
 
 class Minotar {
-	
-	public function get($username) {
-		if(!file_exists('./minecraft/skins/'.strtolower($username).'.png')) {
-			$contents = self::fetch('http://s3.amazonaws.com/MinecraftSkins/'.$username.'.png');
-			if($contents === false) {
-				$img = WideImage::load("http://s3.amazonaws.com/MinecraftSkins/char.png");
-				$img->saveToFile("./minecraft/skins/char.png");
-				header("Status: 404 Not Found");
-				return 'char';
-			} else {
-				$img = WideImage::load($contents);
-				$img->saveToFile('./minecraft/skins/'.strtolower($username).'.png');
-				$helm = clone $img;
-				$helm->crop(40,8,8,8)->saveToFile('./minecraft/helms/'.strtolower($username).'.png');
-				$head = clone $img;
-				$head->crop(8,8,8,8)->saveToFile('./minecraft/heads/'.strtolower($username).'.png');
-				return strtolower($username);
-			}
-		} else {
-			return strtolower($username);
+	const DEFAULT_SKIN = 'char';
+
+
+	public static function get($username) {
+		$lowercase = strtolower($username);
+		
+		$skin = './minecraft/skins/'.$lowercase.'.png';
+		if(file_exists($skin)) return $lowercase;
+
+		$binary = self::fetch('http://s3.amazonaws.com/MinecraftSkins/'.$username.'.png');
+		if($binary === false) {
+			header('Status: 404 Not Found');
+			return ($username == self::DEFAULT_SKIN)
+				? $lowercase
+				: self::get(self::DEFAULT_SKIN);
 		}
+
+		$img = new Imagick();
+		$img->readImageBlob($binary);
+		$img->stripImage(); // strip metadata
+		$img->writeImage($skin);
+
+		$helm = clone $img;
+		$helm->cropImage(8, 8, 40, 8);
+		$helm->writeImage('./minecraft/helms/'.$lowercase.'.png');
+
+		$head = clone $img;
+		$head->cropImage(8, 8, 8, 8);
+		$head->writeImage('./minecraft/heads/'.strtolower($username).'.png');
+
+		return $lowercase;
 	}
 
-	public function getFilesFromDir($dir) { 
+	public static function getFilesFromDir($dir) { 
 		$files = array(); 
 		if ($handle = opendir($dir)) { 
 			while (false !== ($file = readdir($handle))) { 
@@ -42,10 +52,10 @@ class Minotar {
 			} 
 			closedir($handle); 
 		} 
-		return Minotar::array_flat($files); 
+		return self::array_flat($files); 
 	} 
 
-	private function array_flat($array) { 
+	private static function array_flat($array) { 
 		foreach($array as $a) { 
 			if(is_array($a)) { 
 				$tmp = array_merge($tmp, array_flat($a)); 
@@ -57,22 +67,7 @@ class Minotar {
 		return $tmp; 
 	} 
 
-	private function found($url) {
-		$handle = curl_init($url);
-		if (false === $handle) {
-			return false;
-		}
-		curl_setopt($handle, CURLOPT_HEADER, false);
-		curl_setopt($handle, CURLOPT_FAILONERROR, true);  // this works
-		curl_setopt($handle, CURLOPT_HTTPHEADER, Array("User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.15) Gecko/20080623 Firefox/2.0.0.15")); // request as if Firefox    
-		curl_setopt($handle, CURLOPT_NOBODY, true);
-		curl_setopt($handle, CURLOPT_RETURNTRANSFER, false);
-		$connectable = curl_exec($handle);
-		curl_close($handle);
-		return $connectable;
-	}
-
-	private function fetch($url) {
+	private static function fetch($url) {
 		$handle = curl_init($url);
 		if (false === $handle) {
 			return false;
