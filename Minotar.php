feca522b4a4249f06f1c945332a85c9f3192fe3e
Luke Strickland <luke@axxim.net>
2012-08-15 19:50:59 -0500

    Converted back to WideImage

--- Minotar.php
+++ Minotar.php
@@ -1,136 +1,95 @@
 <?php
 
-$config['caching'] = true;
-
 class Minotar {
-	const DEFAULT_SKIN = 'char';
 
-	public static $expires = 86400; // 24 hours in seconds
+    const DEFAULT_SKIN = 'char';
+    const CACHING = true;
 
+    public static $expires = 86400; // 24 hours in seconds
 
-	public static function get($username) {
-		$lowercase = strtolower($username);
-		
-		$skin = './minecraft/skins/'.$lowercase.'.png';
-		if(file_exists($skin)) {
-			if(time() - filemtime($skin) < self::$expires) return $lowercase;
-			$cached = true;
-		}
+    public static function get($username) {
+        if (!file_exists('./minecraft/skins/' . strtolower($username) . '.png')) {
+            $contents = self::fetch('http://s3.amazonaws.com/MinecraftSkins/' . $username . '.png');
+            if ($contents === false) {
+                $img = WideImage::load("http://s3.amazonaws.com/MinecraftSkins/char.png");
+                $img->saveToFile("./minecraft/skins/char.png");
+                $helm = clone $img;
+                $helm->crop(40, 8, 8, 8)->saveToFile('./minecraft/helms/char.png');
+                $head = clone $img;
+                $head->crop(8, 8, 8, 8)->saveToFile('./minecraft/heads/char.png');
+                header("Status: 404 Not Found");
+                return 'char';
+            } else {
+                $img = WideImage::load($contents);
+                $img->saveToFile('./minecraft/skins/' . strtolower($username) . '.png');
+                $helm = clone $img;
+                $helm->crop(40, 8, 8, 8)->saveToFile('./minecraft/helms/' . strtolower($username) . '.png');
+                $head = clone $img;
+                $head->crop(8, 8, 8, 8)->saveToFile('./minecraft/heads/' . strtolower($username) . '.png');
+                return strtolower($username);
+            }
+        } else {
+            return strtolower($username);
+        }
+    }
 
-		$binary = self::fetch('http://s3.amazonaws.com/MinecraftSkins/'.$username.'.png');
-		if($binary === false) {
-			if($cached) return $lowercase;
+    public function getFilesFromDir($dir) {
+        $files = array();
+        if ($handle = opendir($dir)) {
+            while (false !== ($file = readdir($handle))) {
+                if ($file != "." && $file != ".." && $file != "Thumbs.db") {
+                    if (is_dir($dir . '/' . $file)) {
+                        $dir2 = $dir . '/' . $file;
+                        $files[] = getFilesFromDir($dir2);
+                    } else {
+                        $files[] = $dir . '/' . $file;
+                    }
+                }
+            }
+            closedir($handle);
+        }
+        return Minotar::array_flat($files);
+    }
 
-			header('Status: 404 Not Found');
-			return ($username == self::DEFAULT_SKIN)
-				? $lowercase
-				: self::get(self::DEFAULT_SKIN);
-		}
+    private function array_flat($array) {
+        foreach ($array as $a) {
+            if (is_array($a)) {
+                $tmp = array_merge($tmp, array_flat($a));
+            } else {
+                $tmp[] = $a;
+            }
+        }
 
-		$img = new Imagick();
-		$img->readImageBlob($binary);
-		$img->stripImage(); // strip metadata
-		$img->writeImage($skin);
+        return $tmp;
+    }
 
-		self::_getHelm($img)->writeImage('./minecraft/helms/'.$lowercase.'.png');
-		self::_getHead($img)->writeImage('./minecraft/heads/'.$lowercase.'.png');
-		self::_getPlayer($img)->writeImage('./minecraft/players/'.$lowercase.'.png');
+    private function found($url) {
+        $handle = curl_init($url);
+        if (false === $handle) {
+            return false;
+        }
+        curl_setopt($handle, CURLOPT_HEADER, false);
+        curl_setopt($handle, CURLOPT_FAILONERROR, true);  // this works
+        curl_setopt($handle, CURLOPT_HTTPHEADER, Array("User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.15) Gecko/20080623 Firefox/2.0.0.15")); // request as if Firefox    
+        curl_setopt($handle, CURLOPT_NOBODY, true);
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, false);
+        $connectable = curl_exec($handle);
+        curl_close($handle);
+        return $connectable;
+    }
 
-		return $lowercase;
-	}
-
-
-	protected static function _getHead(Imagick $skin) {
-		return $skin->getImageRegion(8, 8, 8, 8);
-	}
-
-	protected static function _getHelm(Imagick $skin) {
-		$head = self::_getHead($skin);
-
-		// detect helm
-		$bg = $skin->exportImagePixels(0, 0, 1, 1, 'RGB', Imagick::PIXEL_CHAR);
-		$pixels = $skin->exportImagePixels(40, 8, 8, 8, 'RGB', Imagick::PIXEL_CHAR);
-		
-		foreach(array_chunk($pixels, 3) as $pixel) {
-			if($pixel === $bg) continue;
-
-			// draw helm
-			$helm = $skin->getImageRegion(8, 8, 40, 8);
-			$head->compositeImage($helm, Imagick::COMPOSITE_DEFAULT, 0, 0);
-			break;
-		}
-
-		return $head;
-	}
-
-	protected static function _getPlayer(Imagick $skin) {
-		$player = new Imagick();
-		$player->newImage(16, 32, 'transparent', 'png');
-
-		// draw helm
-		$player->compositeImage(self::_getHelm($skin), Imagick::COMPOSITE_DEFAULT, 4, 0 );
-		// draw body
-		$part = $skin->getImageRegion(8, 12, 20, 20);
-		$player->compositeImage($part, Imagick::COMPOSITE_DEFAULT, 4, 8);
-		// draw arms
-		$part = $skin->getImageRegion(4, 12, 44, 20);
-		$player->compositeImage($part, Imagick::COMPOSITE_DEFAULT, 0, 8);
-		$part->flopImage();
-		$player->compositeImage($part, Imagick::COMPOSITE_DEFAULT, 12, 8);
-		// draw legs
-		$part = $skin->getImageRegion(4, 12, 4, 20);
-		$player->compositeImage($part, Imagick::COMPOSITE_DEFAULT, 4, 20);
-		$part->flopImage();
-		$player->compositeImage($part, Imagick::COMPOSITE_DEFAULT, 8, 20);
-		
-		return $player;
-	}
-
-
-	private static function fetch($url) {
-		$handle = curl_init($url);
-		if (false === $handle) {
-			return false;
-		}
-		curl_setopt($handle, CURLOPT_HEADER, false);
-		curl_setopt($handle, CURLOPT_FAILONERROR, true);  // this works
-		curl_setopt($handle, CURLOPT_HTTPHEADER, Array("User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.15) Gecko/20080623 Firefox/2.0.0.15")); // request as if Firefox    
-		curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-		$contents = curl_exec($handle);
-		curl_close($handle);
-		return $contents;
-	}
-
-
-	public static function getFilesFromDir($dir) { 
-		$files = array(); 
-		if ($handle = opendir($dir)) { 
-			while (false !== ($file = readdir($handle))) { 
-				if ($file[0] != "." && $file != "Thumbs.db") { 
-					if(is_dir($dir.'/'.$file)) { 
-						$dir2 = $dir.'/'.$file; 
-						$files[] = getFilesFromDir($dir2); 
-					} 
-					else { 
-						$files[] = $dir.'/'.$file; 
-					} 
-				} 
-			} 
-			closedir($handle); 
-		} 
-		return self::array_flat($files); 
-	} 
-
-	private static function array_flat($array) { 
-		foreach($array as $a) { 
-			if(is_array($a)) { 
-				$tmp = array_merge($tmp, array_flat($a)); 
-			} else { 
-				$tmp[] = $a; 
-			} 
-		} 
-
-		return $tmp; 
-	} 
+    private static function fetch($url) {
+        $handle = curl_init($url);
+        if (false === $handle) {
+            return false;
+        }
+        curl_setopt($handle, CURLOPT_HEADER, false);
+        curl_setopt($handle, CURLOPT_FAILONERROR, true);  // this works
+        curl_setopt($handle, CURLOPT_HTTPHEADER, Array("User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.15) Gecko/20080623 Firefox/2.0.0.15")); // request as if Firefox    
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        $contents = curl_exec($handle);
+        curl_close($handle);
+        return $contents;
+    }
 
 }
--- index.php
+++ index.php
@@ -1,122 +1,125 @@
-<?php require 'klein.php';
+<?php
+require 'klein.php';
 include 'WideImage/WideImage.php';
 include 'Minotar.php';
+
 error_reporting(0);
 
 respond('/', function ($request, $response) {
-	$response->render('html/home.phtml');
+    $response->render('html/home.phtml');
 });
 
 respond('/[avatar|head]/[:username].[:format]?/[:size]?.[:formate]?', function ($request, $response) {
-	$name = $request->param('username', 'char');
-	$size = $request->param('size', 180);
-	$ext  = $request->param('format', '.png');
-	$ext  = $request->param('formate', '.png');
-	list($name) = explode('.', $name);
-	list($size) = explode('.', $size);
-	$size = min(1000, max(1, (int) $size));
+    $name = $request->param('username', 'char');
+    $size = $request->param('size', 180);
+    $ext = $request->param('format', '.png');
+    $ext = $request->param('formate', '.png');
+    list($name) = explode('.', $name);
+    list($size) = explode('.', $size);
+    $size = min(1000, max(1, (int) $size));
 
-	$name = Minotar::get($name);
+    $name = Minotar::get($name);
 
-	$img = WideImage::load("./minecraft/heads/$name.png")->resize($size);
-	$img->output($ext);
+    $img = WideImage::load("./minecraft/heads/$name.png")->resize($size);
+    $img->output($ext);
 });
 
 respond('/helm/[:username].[:format]?/[:size]?.[:formate]?', function ($request, $response) {
-	$name = $request->param('username', 'char');
-	$size = $request->param('size', 180);
-	$ext  = $request->param('format', '.png');
-	$ext  = $request->param('formate', '.png');
-	list($name) = explode('.', $name);
-	list($size) = explode('.', $size);
-	$size = min(1000, max(1, (int) $size));
+    $name = $request->param('username', 'char');
+    $size = $request->param('size', 180);
+    $ext = $request->param('format', '.png');
+    $ext = $request->param('formate', '.png');
+    list($name) = explode('.', $name);
+    list($size) = explode('.', $size);
+    $size = min(1000, max(1, (int) $size));
 
-	$name = Minotar::get($name);
+    $name = Minotar::get($name);
 
-	$img = WideImage::load("./minecraft/helms/$name.png")->resize($size);
-	$img->output($ext);
+    $img = WideImage::load("./minecraft/helms/$name.png")->resize($size);
+    $img->output($ext);
 });
 
 respond('/[player|body]/[:username].[:format]?/[:size]?.[:formate]?', function ($request, $response) {
-	$name = $request->param('username', 'char');
-	$size = $request->param('size', 180);
-	$ext  = $request->param('format', '.png');
-	$ext  = $request->param('formate', '.png');
-	list($name) = explode('.', $name);
-	list($size) = explode('.', $size);
-	$size = min(1000, max(1, (int) $size));
+    $name = $request->param('username', 'char');
+    $size = $request->param('size', 180);
+    $ext = $request->param('format', '.png');
+    $ext = $request->param('formate', '.png');
+    list($name) = explode('.', $name);
+    list($size) = explode('.', $size);
+    $size = min(1000, max(1, (int) $size));
 
-	$name = Minotar::get($name);
+    $name = Minotar::get($name);
 
-	$img = WideImage::load("./minecraft/players/$name.png")->resize($size);
-	$img->output($ext);
+    $img = WideImage::load("./minecraft/players/$name.png")->resize($size);
+    $img->output($ext);
 });
 
 respond('/random/[:size]?.[:format]?', function ($request, $response) {
-	$size = $request->param('size', 180);
+    $size = $request->param('size', 180);
 	$ext  = $request->param('format', '.png');
-	list($size) = explode('.', $size);
-	$size = min(1000, max(1, (int) $size));
+    list($size) = explode('.', $size);
+    $size = min(1000, max(1, (int) $size));
 
-	$avatars = array_diff(scandir('./minecraft/heads/'), array('..', '.'));
-	$rand = array_rand($avatars);
+    $avatars = array_diff(scandir('./minecraft/heads/'), array('..', '.', '.gitignore'));
+    $rand = array_rand($avatars);
 
-	$avatar = $avatars[$rand];
+    $avatar = $avatars[$rand];
 
-	$name = str_replace('.png', '', $avatar);
-	header("Cache-Control: no-cache, must-revalidate");
-	header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); 
-	WideImage::load("./minecraft/heads/$name.png")->resize($size)->output('png');
+    $name = str_replace('.png', '', $avatar);
+    header("Cache-Control: no-cache, must-revalidate");
+    header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+    WideImage::load("./minecraft/heads/$name.png")->resize($size)->output('png');
 });
 
 respond('/download/[:username]', function ($request, $response) {
-	$name = $request->param('username', 'char');
+    $name = $request->param('username', 'char');
 
-	$name = Minotar::get($name);
+    $name = Minotar::get($name);
 
-	$img = WideImage::load("./minecraft/skins/$name.png");
-	header('Content-Disposition: attachment; filename="'.$name.'.png"');
-	$img->output('.png');
-
+    $img = WideImage::load("./minecraft/skins/$name.png");
+    header('Content-Disposition: attachment; filename="' . $name . '.png"');
+    $img->output('.png');
 });
 
 respond('/skin/[:username]', function ($request, $response) {
-	$name = $request->param('username', 'char');
+    $name = $request->param('username', 'char');
 
-	$name = Minotar::get($name);
+    $name = Minotar::get($name);
 
-	$img = WideImage::load("./minecraft/skins/$name.png");
-	$img->output('.png');
+    $img = WideImage::load("./minecraft/skins/$name.png");
+    $img->output('.png');
 });
 
 respond('/all/[:type]?', function ($request, $response) {
-	$type = $request->param('type', 'heads');
-	$files = Minotar::getFilesFromDir("./minecraft/$type");
-	$response->render('html/all.phtml', array('files' => $files));
+    $type = $request->param('type', 'heads');
+    $files = Minotar::getFilesFromDir("./minecraft/$type");
+    $response->render('html/all.phtml', array('files' => $files));
 });
 
 respond('/wallpaper/[:width]/[:height]?', function ($request, $response) {
-	$width = $request->param('width', 1024);
-	$height = $request->param('height', 768);
-	$files = Minotar::getFilesFromDir("./minecraft/heads");
-	if($width >= 1920) $width = 1920;
-	if($height >= 1080) $height = 1080;
-	$files = array_slice($files, 500);
+    $width = $request->param('width', 1024);
+    $height = $request->param('height', 768);
+    $files = Minotar::getFilesFromDir("./minecraft/heads");
+    if ($width >= 1920)
+        $width = 1920;
+    if ($height >= 1080)
+        $height = 1080;
+    $files = array_slice($files, 500);
 
-	//list($width, $height) = getimagesize($_GET['image']);
-	$image_p = imagecreatetruecolor($width, $height);
-	$count = 1;
-	foreach($files as $avatar) {
-		$image = imagecreatefrompng($avatar);
-		imagecopyresampled($image_p, $image, $width * $count, $height * $count, 0, 0, $width, $height, 42, 42);
-		$count++;
-	}
-	header('Content-type: image/png');
-	imagejpeg($image_p, null, 100);
+    //list($width, $height) = getimagesize($_GET['image']);
+    $image_p = imagecreatetruecolor($width, $height);
+    $count = 1;
+    foreach ($files as $avatar) {
+        $image = imagecreatefrompng($avatar);
+        imagecopyresampled($image_p, $image, $width * $count, $height * $count, 0, 0, $width, $height, 42, 42);
+        $count++;
+    }
+    header('Content-type: image/png');
+    imagejpeg($image_p, null, 100);
 });
 
 respond('404', function ($request, $response) {
-        $response->render('html/404.phtml');
+    $response->render('html/404.phtml');
 });
 
-dispatch(); 
+dispatch();
