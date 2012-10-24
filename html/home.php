9dae683a9bcfed5e41b5d6700b847455c99ef3c4
Luke Strickland <luke@axxim.net>
2012-10-24 10:44:23 -0500

    Removed advertisement and changed host

--- html/home.php
+++ html/home.php
@@ -1,7 +1,15 @@
 <div class="fifteen columns">
     <h1><a href="/">Minotar</a></h1>
 
-    <h5 class="center">A Minotar is a global avatar that pulls your head off your Minecraft.net skin, <br>and allows it for use on thousands of sites.</h5>
+    <div class="twelve columns">
+        <h5 class="center">A Minotar is a global avatar that pulls your head off your Minecraft.net skin, <br>and allows it
+            for use on thousands of sites.</h5>
+    </div>
+    <div class="two columns">
+        <span class="gittip-0001" gittip-username="clone1018"></span>
+        <script id="gittip-0001" src="https://www.gittip.com/assets/widgets/0001.js"></script>
+    </div>
+    <div class="clearfix"></div>
 
     <hr>
 
@@ -19,18 +27,6 @@
         <li class="head"><img src="<?php echo URL; ?>avatar/ez/64.png" alt="ez"></li>
     </ul>
 
-     <div style="text-align: center">
-        <script type="text/javascript"><!--
-            google_ad_client = "ca-pub-5632175804795221";
-            /* Minotar Top */
-            google_ad_slot = "4748997833";
-            google_ad_width = 728;
-            google_ad_height = 90;
-            //-->
-        </script>
-        <script type="text/javascript" src="https://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
-    </div>
-
     <br class="clear"><br>
 </div>
 <div class="eight columns content">
@@ -39,24 +35,29 @@
     <hr>
 
     <h4>Simple Heads</h4>
+
     <p>Unlike the PayPal API, we keep things nice and simple. For basic usage just provide a username:</p>
     <pre>&#60;img src="https://minotar.net/avatar/<span class="blue">clone1018</span>"&#62;</pre>
 
-    <p>You can also set a size. We use pixels and we only need the <span class="red">width</span>. Just add it to the end.</p>
+    <p>You can also set a size. We use pixels and we only need the <span class="red">width</span>. Just add it to the
+        end.</p>
     <pre>&#60;img src="https://minotar.net/avatar/<span class="blue">clone1018</span>/<span class="red">100</span>"&#62;</pre>
 
-    <p>And because some services require an extension, we've added simple support for it. Just add <span class="green">.png</span> to the end.</p>
+    <p>And because some services require an extension, we've added simple support for it. Just add <span class="green">.png</span>
+        to the end.</p>
     <pre>&#60;img src="https://minotar.net/avatar/<span class="blue">clone1018</span>/<span class="red">100</span><span class="green">.png</span>"&#62;</pre>
 
     <br>
 
     <h4>Avatar With Helm</h4>
+
     <p>Sometimes you want to display a helm too, that's fine with this endpoint:</p>
     <pre>&#60;img src="https://minotar.net/helm/<span class="blue">clone1018</span>/<span class="red">100</span><span class="green">.png</span>"&#62;</pre>
 
     <br>
 
     <h4>User's Skin</h4>
+
     <p>You can even use Minotar's API to get a users skin. <em>We're adding more soon!</em></p>
     <pre>&#60;img src="https://minotar.net/skin/<span class="blue">clone1018</span>"&#62;</pre>
 
@@ -66,27 +67,34 @@
     <br>
 
     <h4>Random Skin</h4>
+
     <p>Randomly generated head? We got that too.</p>
     <pre>&#60;img src="https://minotar.net/random/<span class="red">100</span><span class="green">.png</span>"&#62;</pre>
 
     <br>
 
     <h4>View all Minotars</h4>
-    <p>You can browse through all the <a href="<?php echo URL; ?>all/skin">skins</a>, <a href="<?php echo URL; ?>all/head">heads</a> and <a href="<?php echo URL; ?>all/helm">helms</a>.</p>
+
+    <p>You can browse through all the <a href="<?php echo URL; ?>all/skin">skins</a>,
+        <a href="<?php echo URL; ?>all/head">heads</a> and <a href="<?php echo URL; ?>all/helm">helms</a>.</p>
 
     <br>
 
     <h4>Refresh your Minotar</h4>
-    <p>If you have changed your skin you can force Minotar to update your avatar by visiting the "refresh" link in your browser:</p>
+
+    <p>If you have changed your skin you can force Minotar to update your avatar by visiting the "refresh" link in your
+        browser:</p>
     <pre>https://minotar.net/refresh/<span class="blue">clone1018</span></pre>
 
     <br>
 
     <h4>Default Skin</h4>
+
     <p>Need Steve? Use "char" as the username:</p>
     <pre>&#60;img src="https://minotar.net/avatar/<span class="blue">char</span>"&#62;</pre>
 
-    <br><hr>
+    <br>
+    <hr>
 
 </div>
 <div class="six columns offset-by-one hidden-phone" id="right">
@@ -97,21 +105,30 @@
 
     <ul>
         <li><strong><a href="http://sushigaming.com/">Sushigaming</a></strong><br>
-            Awesome sponsors of Minotar.net!</li>
+            Awesome sponsors of Minotar.net!
+        </li>
         <li><a href="http://www.minecraftforum.net/forum">Minecraft Forum</a><br>
-            Official Minecraft Forum!</li>
+            Official Minecraft Forum!
+        </li>
         <li><a href="http://www.enjin.com/">Enjin</a><br>
-            Guild Website Hosting.</li>
-        <li><a href="http://forums.bukkit.org/threads/econ-web-virtualshop-v2-3-fight-communism-with-free-market-1-shop-plugin-now-with-webui-1000.18948/">VirtualShop</a><br>
-            Virtual Store plugin for Bukkit.</li>
+            Guild Website Hosting.
+        </li>
+        <li>
+            <a href="http://forums.bukkit.org/threads/econ-web-virtualshop-v2-3-fight-communism-with-free-market-1-shop-plugin-now-with-webui-1000.18948/">VirtualShop</a><br>
+            Virtual Store plugin for Bukkit.
+        </li>
         <li><a href="http://minecraft-skins.com/">Minecraft Skins</a><br>
-            The largest skin browsing site.</li>
+            The largest skin browsing site.
+        </li>
         <li><a href="http://yeahwh.at/">YEAHWH.AT?!</a><br>
-            A large Minecraft server.</li>
+            A large Minecraft server.
+        </li>
         <li><a href="http://massivecraft.com/">MassiveCraft</a><br>
-            Massivecraft is a medieval fantasy role-playing Minecraft server.</li>
+            Massivecraft is a medieval fantasy role-playing Minecraft server.
+        </li>
         <li><a href="http://www.minecraftnation.org/">Minecraft Nation</a><br>
-            We're a medium sized Minecraft community that values cooperation and teamwork.</li>
+            We're a medium sized Minecraft community that values cooperation and teamwork.
+        </li>
     </ul>
 
     <br>
@@ -127,14 +144,11 @@
         <li><a href="http://vanillaforums.org/addon/minecraftatar-plugin/">Vanilla Forum Plugin</a></li>
     </ul>
 
-    <iframe style="border:0;margin:0;padding:0;" src="https://www.gittip.com/clone1018/widget.html" width="48" height="20"></iframe><br>
-    Like Minotar? Tip Me!
-
     <hr>
 
     <footer>
         <p>Copyright 2012 &ndash; <a href="http://axxim.net">Axxim, LLC</a><br>
-        Bandwidth provided by <a href="http://vpsno.de">VPSNode</a></p>
+            Hosted by <a href="https://www.digitalocean.com/">DigitalOcean</a></p>
 
         <p>Source code is on <a href="https://github.com/Axxim/Minotar">GitHub</a></p>
     </footer>
