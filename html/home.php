c080e5944605a5450da48b19e120036446b3cca0
Luke Strickland <luke@axxim.net>
2012-11-30 10:45:26 -0600

    Added PlanetMinecraft
    They use Minotar on http://www.planetminecraft.com/chat/

--- html/home.php
+++ html/home.php
@@ -104,14 +104,17 @@
     <hr>
 
     <ul>
-        <li><strong><a href="http://sushigaming.com/">Sushigaming</a></strong><br>
-            Awesome sponsors of Minotar.net!
-        </li>
         <li><a href="http://www.minecraftforum.net/forum">Minecraft Forum</a><br>
             Official Minecraft Forum!
         </li>
         <li><a href="http://www.enjin.com/">Enjin</a><br>
             Guild Website Hosting.
+        </li>
+        <li><a href="http://www.planetminecraft.com/">PlanetMinecraft</a><br>
+            The largest Minecraft resource site.
+        </li>
+        <li><a href="http://sushigaming.com/">Sushigaming</a><br>
+            Awesome sponsors of Minotar.net!
         </li>
         <li>
             <a href="http://forums.bukkit.org/threads/econ-web-virtualshop-v2-3-fight-communism-with-free-market-1-shop-plugin-now-with-webui-1000.18948/">VirtualShop</a><br>
