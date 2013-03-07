84bcd278652910747b8f0d521667bc5bee6039d2
Alex Gittemeier <algitt65@gmail.com>
2013-03-07 16:45:31 -0600

    Added support for directory location independency.
    The require 'vendor/portable.php' should set the variable $pkg_root to
    dirname($_SERVER['PHP_SELF']) or the CGI equivalent.

--- html/404.php
+++ html/404.php
@@ -1,5 +1,8 @@
+<?php
+require 'vendor/portable.php';
+?>
 <div class="span12 content">
     <h1>404</h1>
     <p class="center">The requested content cannot be found</p>
-    <p class="center"><a href="/">&larr; Return to Minotar</a></p>
+    <p class="center"><a href="<?php echo $pkg_root; ?>">&larr; Return to Minotar</a></p>
 </div>
