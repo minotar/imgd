62ae009621f687b58dc8bb39cc2f61c8ad7c86e9
LukeHandle <luke.handle@googlemail.com>
2014-12-06 21:03:53 +0000

    Basic benchmarking
    "go test -bench=."

--- main_test.go
+++ main_test.go
@@ -1,7 +1,37 @@
 package main
 
-import "testing"
+import (
+	"github.com/minotar/minecraft"
+	"testing"
+)
 
 func TestNothing(t *testing.T) {
 
+}
+
+func BenchmarkGetHead(b *testing.B) {
+	skin := fetchSkin("clone1018")
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		GetHead(skin)
+	}
+}
+
+func BenchmarkGetHelm(b *testing.B) {
+	skin := fetchSkin("clone1018")
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		GetHelm(skin)
+	}
+}
+
+func BenchmarkGetBody(b *testing.B) {
+	skin := fetchSkin("clone1018")
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		GetBody(skin)
+	}
 }
