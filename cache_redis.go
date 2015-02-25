ca1597b7e6d8a9f96adc43959a9d64cd4e6e36ab
LukeHandle <luke.handle@googlemail.com>
2015-02-25 00:08:19 +0000

    Add more info to the Redis start log

--- cache_redis.go
+++ cache_redis.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"errors"
-	"fmt"
 	"image/png"
 	"strconv"
 	"strings"
@@ -56,7 +55,7 @@ func (c *CacheRedis) setup() error {
 
 	c.Pool = pool
 
-	log.Info("Loaded Redis cache (pool: " + fmt.Sprintf("%v", config.Redis.PoolSize) + ")")
+	log.Info("Loaded Redis cache (address: %s, db: %v, prefix: \"%s\", pool: %v)", config.Redis.Address, config.Redis.DB, config.Redis.Prefix, config.Redis.PoolSize)
 	return nil
 }
 
