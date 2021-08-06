01382e2bd43d664e5bdeacc1dfd7bf27564599b3
Luke Hanley <luke.handle@googlemail.com>
2021-08-06 17:08:55 +0100

    McClient: cleanup logging, review logic

--- pkg/mcclient/cache.go
+++ pkg/mcclient/cache.go
@@ -2,6 +2,7 @@ package mcclient
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/minotar/imgd/pkg/cache"
 	"github.com/minotar/imgd/pkg/mcclient/mcuser"
@@ -11,13 +12,13 @@ import (
 
 // Todo: metrics / tracing / timing
 
-// Todo: these should all be lowercasing the Username/UUID before retrieving/inserting caches
-
 // Todo: Also, add function context to logger
 // User cache name and UUID in logger.With()?
 
 // CacheRetrieveUUIDEntry searches the cache based on Username, expecting a UUID in response
 func (mc *McClient) CacheRetrieveUUIDEntry(logger log.Logger, username string) (uuidEntry uuid.UUIDEntry, err error) {
+	// logger should already be With() the username
+	username = strings.ToLower(username)
 	// Metrics timer / tracing
 	// Though - is this useless when using a TieredCache which is inherentantly varied?
 	uuidBytes, err := mc.Caches.UUID.Retrieve(username)
@@ -26,11 +27,11 @@ func (mc *McClient) CacheRetrieveUUIDEntry(logger log.Logger, username string) (
 		// Return an error (and log based on severity)
 		if err == cache.ErrNotFound {
 			// Metrics stat "Miss"
-			logger.Debugf("Did not find username \"%s\" in %s", username, mc.Caches.UUID.Name())
+			logger.Debugf("Did not find username in %s", mc.Caches.UUID.Name())
 			return
 		} else {
 			// Metrics stat Cache RetrieveError
-			logger.Errorf("Failed to lookup up username \"%s\" in %s: %v", username, mc.Caches.UUID.Name(), err)
+			logger.Errorf("Failed to lookup up username in %s: %v", mc.Caches.UUID.Name(), err)
 			return
 		}
 	}
@@ -38,14 +39,14 @@ func (mc *McClient) CacheRetrieveUUIDEntry(logger log.Logger, username string) (
 	if len(uuidBytes) < 5 {
 		// 4 bytes or less would be an invalid status/timestamp
 		// Metrics stats Cache Empty Error
-		logger.Errorf("Null UUID returned for username \"%s\" in %s: %v", username, mc.Caches.UUID.Name(), uuidBytes)
+		logger.Errorf("Null UUID returned for username in %s: %v", mc.Caches.UUID.Name(), uuidBytes)
 		return uuid.UUIDEntry{}, fmt.Errorf("cache returned null UUID")
 	}
 
 	uuidEntry = uuid.DecodeUUIDEntry(uuidBytes)
 
 	// Metrics stat Hit
-	logger.Debugf("Found username \"%s\" in %s: %v", username, mc.Caches.UUID.Name(), uuidEntry)
+	logger.Debugf("Found username in %s", mc.Caches.UUID.Name())
 	return
 }
 
@@ -53,6 +54,8 @@ func (mc *McClient) CacheRetrieveUUIDEntry(logger log.Logger, username string) (
 // There is no sanity checking on the input
 // The item is cached witha TTL assuming it's brand-new
 func (mc *McClient) CacheInsertUUIDEntry(logger log.Logger, username string, uuidEntry uuid.UUIDEntry) (err error) {
+	// logger should already be With() the username and UUID
+	username = strings.ToLower(username)
 	// Technically this could be empty / nil???
 	uuidBytes := uuidEntry.Encode()
 
@@ -62,12 +65,14 @@ func (mc *McClient) CacheInsertUUIDEntry(logger log.Logger, username string, uui
 	// Observe Cache insert
 	if err != nil {
 		// stats.CacheUUID("error")
-		logger.Errorf("Failed Insert username:UUID into cache (%s:%s): %v", username, uuidEntry, err)
+		logger.Errorf("Failed Insert into cache %s: %v", mc.Caches.UUID.Name(), err)
 	}
 	return
 }
 
 func (mc *McClient) CacheRetrieveMcUser(logger log.Logger, uuid string) (user mcuser.McUser, err error) {
+	// logger should already be With() the UUID (and maybe username)
+	uuid = strings.ToLower(uuid)
 	// Metrics timer / tracing
 	// Though - is this useless when using a TieredCache which is inherentantly varied?
 	userBytes, err := mc.Caches.UserData.Retrieve(uuid)
@@ -76,31 +81,33 @@ func (mc *McClient) CacheRetrieveMcUser(logger log.Logger, uuid string) (user mc
 		// Return an error (and log based on severity)
 		if err == cache.ErrNotFound {
 			// Metrics stat "Miss"
-			logger.Debugf("Did not find uuid \"%s\" in %s", uuid, mc.Caches.UserData.Name())
+			logger.Debugf("Did not find uuid in %s", mc.Caches.UserData.Name())
 			return
 		} else {
 			// Metrics stat Cache RetrieveError
-			logger.Errorf("Failed to lookup up uuid \"%s\" in %s: %v", uuid, mc.Caches.UserData.Name(), err)
+			logger.Errorf("Failed to lookup up uuid in %s: %v", mc.Caches.UserData.Name(), err)
 			return
 		}
 	}
 
 	user, err = mcuser.DecompressMcUser(userBytes)
 	if err != nil {
-		logger.Errorf("Failed to decode user from uuid \"%s\" in %s: %v", uuid, mc.Caches.UserData.Name(), err)
+		logger.Errorf("Failed to decode McUser from uuid in %s: %v", mc.Caches.UserData.Name(), err)
 		// Metrics stats Cache Decode Error
 		return
 	}
 	// Metrics stat Hit
-	logger.Debugf("Found uuid \"%s\" in %s: %s", uuid, mc.Caches.UUID.Name(), uuid)
+	logger.Debugf("Found uuid in %s", mc.Caches.UserData.Name())
 	return
 }
 
 func (mc *McClient) CacheInsertMcUser(logger log.Logger, uuid string, user mcuser.McUser) (err error) {
+	// logger should already be With() the UUID (and maybe username)
+	uuid = strings.ToLower(uuid)
 	packedUserBytes, err := user.Compress()
 	if err != nil {
 		// stats.CacheUser("pack_error")
-		logger.Errorf("Failed to pack UUID:user ready to cache (%s:%s): %v", uuid, user.Username, err)
+		logger.Errorf("Failed to pack UUID:user ready to cache: %v", err)
 	}
 
 	// Metrics timer / tracing
@@ -109,7 +116,7 @@ func (mc *McClient) CacheInsertMcUser(logger log.Logger, uuid string, user mcuse
 	// Observe Cache insert
 	if err != nil {
 		// stats.CacheUser("insert_error")
-		logger.Errorf("Failed Insert UUID:user into cache (%s:%s): %v", uuid, user.Username, err)
+		logger.Errorf("Failed Insert UUID:user into cache %s: %v", mc.Caches.UserData.Name(), err)
 	}
 	return
 }
