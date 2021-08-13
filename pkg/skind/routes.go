d93d31da0e26ce13fb906731bd40556d08734cf7
Luke Hanley <luke.handle@googlemail.com>
2021-08-13 17:09:23 +0100

    Skind: add skin & download & redirect

--- pkg/skind/routes.go
+++ pkg/skind/routes.go
@@ -1,17 +1,37 @@
 package skind
 
 import (
+	"image/png"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
 	"github.com/minotar/imgd/pkg/mcclient"
+	"github.com/minotar/minecraft"
 )
+
+
+func CorsHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func BrowserDownloadHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Todo: pretty download name would be nice...
+		w.Header().Add("Content-Disposition", "attachment; filename=\"skin.png\"")
+		next.ServeHTTP(w, r)
+	})
+}
 
 // Requires "uuid" or "username" vars
 func SkinPageHandler(skind *Skind) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		logger := skind.Cfg.Logger
-		logger.Info("Incoming Request!!!!")
 
 		var userReq mcclient.UserReq
 		vars := mux.Vars(r)
@@ -28,6 +48,24 @@ func SkinPageHandler(skind *Skind) http.Handler {
 
 		logger.Infof("User hash is: %s", skin.Hash)
 
-		w.WriteHeader(200)
+		// No more header changes after writing
+		WriteSkin(w, skin)
+		logger.Debug(w.Header())
+	})
+}
+
+func WriteSkin(w http.ResponseWriter, skin minecraft.Skin) {
+	// Todo: do we still want to use Skin Hash
+	eTag := skin.Hash
+	w.Header().Add("ETag", eTag)
+	w.Header().Add("Content-Type", "image/png")
+	png.Encode(w, skin.Image)
+}
+
+func DashedRedirectUUIDHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Todo: log stat
+		dst := strings.Replace(r.URL.Path, "-", "", 4)
+		http.Redirect(w, r, dst, http.StatusMovedPermanently)
 	})
 }
