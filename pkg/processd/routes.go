d0fe2f65538892cd594cb7fcdbfae4006258a893
Luke Hanley <luke.handle@googlemail.com>
2021-09-05 13:03:41 +0100

    restruct McSkin as http.Handler

--- pkg/processd/routes.go
+++ pkg/processd/routes.go
@@ -2,45 +2,12 @@ package processd
 
 import (
 	"net/http"
-	"strconv"
 	"strings"
 
 	"github.com/gorilla/mux"
 	"github.com/minotar/imgd/pkg/util/route_helpers"
 	"github.com/minotar/minecraft"
 )
-
-// Set the default, min and max width to resize processed images to
-const (
-	DefaultWidth = int(180)
-	MinWidth     = int(8)
-	MaxWidth     = int(300)
-)
-
-// GetWidth converts and sanitizes the string for the avatar width.
-func GetWidth(inp string) int {
-	out, err := strconv.Atoi(inp)
-	if err != nil {
-		return DefaultWidth
-	} else if out > MaxWidth {
-		return MaxWidth
-	} else if out < MinWidth {
-		return MinWidth
-	}
-	return out
-
-}
-
-func GetHead(skin minecraft.Skin) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		width := GetWidth(vars["width"])
-
-		workingSkin := mcSkin{Skin: skin}
-		workingSkin.GetHead(width)
-		workingSkin.WritePNG(w)
-	})
-}
 
 type SkinProcessor func(minecraft.Skin) http.Handler
 
