718213416c6747e5d1f673ab263ad9b17039811a
Luke Hanley <luke.handle@googlemail.com>
2021-10-23 21:37:24 +0100

    libdeflate: fix handling when data was tiny and got bigger

--- pkg/util/flate/libdeflate_cgo.go
+++ pkg/util/flate/libdeflate_cgo.go
@@ -10,6 +10,11 @@ import "github.com/4kills/go-libdeflate"
 
 func Compress(in []byte) ([]byte, error) {
 	_, out, err := libdeflate.Compress(in, nil, libdeflate.ModeDEFLATE)
+	if err != nil && err.Error() == "libdeflate: native: compressed data is much larger than uncompressed" {
+		// libdeflate complains if the compression made it bigger
+		// We "probably" don't care as it was likely a 15 byte invalid user
+		return out, nil
+	}
 	return out, err
 }
 
